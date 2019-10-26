5614001ebfdf3b107b06724c7bde656d873b6109
Taha Khozooie <taha@hostiran.com>
2019-10-26 14:39:29 +0330

    Updates

---
+++ precache-manifest.f6eb4891e6788d7efa4b335ba756c141.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f2e34f25cf8a7c29cb308aa7b39120",
+    "url": "/developteam/index.html"
+  },
+  {
+    "revision": "6602f21350076c340668",
+    "url": "/developteam/static/css/4.30b40c00.chunk.css"
+  },
+  {
+    "revision": "f3ff4416128b149e8bad",
+    "url": "/developteam/static/css/5.dd3357ac.chunk.css"
+  },
+  {
+    "revision": "77f428b4f6090f46dfcb",
+    "url": "/developteam/static/css/main.37dce4a2.chunk.css"
+  },
+  {
+    "revision": "4ac3a26769fc65a66168",
+    "url": "/developteam/static/js/2.d7b48e0a.chunk.js"
+  },
+  {
+    "revision": "72af7817b7c046e8c7e3",
+    "url": "/developteam/static/js/3.88ab5de6.chunk.js"
+  },
+  {
+    "revision": "6602f21350076c340668",
+    "url": "/developteam/static/js/4.f36cf996.chunk.js"
+  },
+  {
+    "revision": "f3ff4416128b149e8bad",
+    "url": "/developteam/static/js/5.6cdc2b21.chunk.js"
+  },
+  {
+    "revision": "77f428b4f6090f46dfcb",
+    "url": "/developteam/static/js/main.ac2bfdd1.chunk.js"
+  },
+  {
+    "revision": "0d4aa40841f1eb7f3150",
+    "url": "/developteam/static/js/runtime-main.a312c859.js"
+  },
+  {
+    "revision": "3ca37ae1d895a081fe9874e50bd71694",
+    "url": "/developteam/static/media/CoinIXLogo.3ca37ae1.png"
+  },
+  {
+    "revision": "c49aa28562141580e5560b32bd9fa7b2",
+    "url": "/developteam/static/media/fclogo.c49aa285.png"
+  },
+  {
+    "revision": "d0915bc3877280aee83c4d1cd3bc3856",
+    "url": "/developteam/static/media/gmail.d0915bc3.svg"
+  },
+  {
+    "revision": "47a183032adac8791d29f6cd776b4ac3",
+    "url": "/developteam/static/media/headerImage.47a18303.png"
+  },
+  {
+    "revision": "622e2947df6ed5a12b06a6ea7cb70777",
+    "url": "/developteam/static/media/instagram.622e2947.svg"
+  },
+  {
+    "revision": "c1eb27404bbc0d6052620ac79194ae19",
+    "url": "/developteam/static/media/linkedin.c1eb2740.svg"
+  },
+  {
+    "revision": "91faa7fc44722b2fa15f7bebe04349f6",
+    "url": "/developteam/static/media/maglogo.91faa7fc.png"
+  },
+  {
+    "revision": "d3b53971ced8dd5a1878990b0b3404b9",
+    "url": "/developteam/static/media/photopuzzlylogo.d3b53971.png"
+  },
+  {
+    "revision": "6d23f36111ac3d9da7dff1150db444c6",
+    "url": "/developteam/static/media/skillImage.6d23f361.png"
+  },
+  {
+    "revision": "75a50e92b7b9a680d3765ca1bfd10dd1",
+    "url": "/developteam/static/media/soillogo.75a50e92.png"
+  },
+  {
+    "revision": "1330ba39ac54aa1c67c7f6f12db4af20",
+    "url": "/developteam/static/media/telegramlogo.1330ba39.png"
+  },
+  {
+    "revision": "838504d11d1214f41baaa4e90c1ff472",
+    "url": "/developteam/static/media/twentylogo.838504d1.png"
+  },
+  {
+    "revision": "ca3063136a95c7ba42f048d947dcb98d",
+    "url": "/developteam/static/media/twitter.ca306313.svg"
+  }
+]);
